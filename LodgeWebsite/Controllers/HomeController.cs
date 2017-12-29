0f4775a0b5f3c8f7f7791ee6e7b7394261703bd8
Christopher Smith <csmith4work@gmail.com>
2017-12-29 16:33:24 -0600

    Add project files.

---
+++ LodgeWebsite/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LodgeWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Officers()
+        {
+            return View();
+        }
+
+        public IActionResult Contributions()
+        {
+            ViewData["Message"] = "CHIPs description page.";
+
+            return View();
+        }
+
+        public IActionResult Bylaws()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
